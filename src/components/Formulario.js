95a3360377f1a73da56b82a5a6f5fcd4edfc8786
Andres Contreras <andres_c8@outlook.com>
2020-02-09 18:02:00 -0500

    calculo de la cotizacion

---
+++ src/components/Formulario.js
@@ -0,0 +1,148 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import { obtenerDiferenciaYear, calcularMarca, obtenerPlan } from '../helper';
+
+/* ===========================================
+Inicio Estilos 
+===========================================*/
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Boton = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  margin-top: 2rem;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #26c6da;
+    cursor: pointer;
+    padding: 1rem;
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+const Error = styled.div`
+  background-color: red;
+  color: white;
+  padding: 1rem;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+/* ===========================================
+FIn Estilos 
+===========================================*/
+
+const Formulario = ({ guardarResumen }) => {
+  const [datos, guardarDatos] = useState({
+    marca: '',
+    year: '',
+    plan: ''
+  });
+
+  const [error, guardarError] = useState(false);
+
+  const { marca, year, plan } = datos;
+
+  const obtenerInformacion = e => {
+    guardarDatos({ ...datos, [e.target.name]: e.target.value });
+  };
+
+  const cotizarSeguro = e => {
+    e.preventDefault();
+    if (marca.trim() === '' || year.trim() === '' || plan.trim() === '') {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+    let resultado = 2000;
+    const diferencia = obtenerDiferenciaYear(year);
+    resultado -= (diferencia * 3 * resultado) / 100;
+    resultado = calcularMarca(marca) * resultado;
+    resultado = parseFloat(obtenerPlan(plan) * resultado).toFixed(2);
+    guardarResumen({
+      cotizacion: resultado,
+      datos
+    });
+  };
+
+  return (
+    <form onSubmit={cotizarSeguro}>
+      {error ? <Error>Campos incompletos</Error> : null}
+      <Campo>
+        <Label>Marca</Label>
+        <Select name="marca" value="value" onChange={obtenerInformacion}>
+          <option value="">--Seleccione--</option>
+          <option value="americano">Americano</option>
+          <option value="europeo">Europeo</option>
+          <option value="asicatico">Asiatico</option>
+        </Select>
+      </Campo>
+      <Campo>
+        <Label>Año</Label>
+        <Select name="year" value="year" onChange={obtenerInformacion}>
+          <option value="">-- Seleccione --</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+          <option value="2014">2014</option>
+          <option value="2013">2013</option>
+          <option value="2012">2012</option>
+        </Select>
+      </Campo>
+      <Campo>
+        <Label>Plan</Label>
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="basico"
+          checked={plan == 'basico'}
+          onChange={obtenerInformacion}
+        />
+        Basico
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="completo"
+          checked={plan == 'completo'}
+          onChange={obtenerInformacion}
+        />
+        Completo
+      </Campo>
+      <Boton type="bubmit">Cotizar</Boton>
+    </form>
+  );
+};
+
+export default Formulario;
---
+++ src/helper.js
@@ -0,0 +1,24 @@
+export function obtenerDiferenciaYear(year) {
+  return new Date().getFullYear - year;
+}
+
+export function calcularMarca(marca) {
+  let incremento;
+  switch (marca) {
+    case 'europeo':
+      incremento = 1.3;
+      break;
+    case 'americano':
+      incremento = 1.15;
+      break;
+    case 'asiatico':
+      incremento = 1.05;
+      break;
+  }
+
+  return incremento;
+}
+
+export function obtenerPlan(plan) {
+  return plan === 'basico' ? 1.2 : 1.5;
+}
